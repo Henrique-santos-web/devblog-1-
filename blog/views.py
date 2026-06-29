885a9ee65549a034517a48b9a8a8adc1cb2e2b04
Henrique Miranda <fegemeos4@gmail.com>
2026-06-29 20:59:57 -0300

    feat: exercício da aula 67 - add página sobre com timeline de aprendizado

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-
     return render(request, "blog/index.html")
 
 def sobre_nos(request):
-    mensagem = "<h1>Sobre o DevBlog!</h1>"
-
-    return HttpResponse(mensagem)
+    return render(request, "blog/sobre.html")
