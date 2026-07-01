01cdf4943fa65f1430361f43dd7ff79d64f80e1b
Henrique Dos Santos Miranda” <fegemeos4@gmail.com>
2026-07-01 15:54:57 -0300

    feat: add models Categoria e Artigo com ForeignKey e migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    #*Aqui estamos colocando o CharField, que serve para usar função max_length, e vai limitar as caracteres (de acordo com o numero que atribuir a ela)
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
+
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=200)
+    #* Sem limite de caracteres
+    conteudo = models.TextField()
+
+    #*Preenche com a data/hora exata do cadastro automáticamente
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+
+    # O que é o on_delete=models.CASCADE?
+    # Define o que acontece com os artigos se a categoria for deletada.
+    # CASCADE significa: "deleta a categoria → deleta todos os artigos dela junto". Como um efeito cascata.
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    #               ↑
+    #    esse campo está dentro do Artigo
+    #    então é o Artigo que aponta pra Categoria
+    #    e não o contrário
+    #    Ou seja, por ForeignKey estar dentro de artigo, o ForeignKey pega o artigo e
+    #    diz que ele pertence à Categoria
+
+    autor = models.CharField(max_length=50, default="Admin")
+
+    def __str__(self):
+        return self.titulo
