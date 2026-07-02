83ebbaf736ba9485474f9ab372381ef79bec8e44
Henrique Miranda <fegemeos4@gmail.com>
2026-07-01 21:33:35 -0300

    feat: Django Amin e Fechando o ciclo de aprendizagem MVT

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
+# Todas as funções que eu quero que o meu usuário Admin execute, eu venho aqui e habilito a ele
 from django.contrib import admin
+from.models import Categoria, Artigo
 
-# Register your models here.
+admin.site.register(Categoria)
+
+@admin.register(Artigo) #* O @... Se chama decoration 
+class ArtigoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'categoria', 'data_publicacao')
+    search_fields = ('titulo', 'conteudo')
+    list_filter = ('categoria', 'data_publicacao')
